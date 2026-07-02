5d423c19963bd890a56409d27735d1b5b8c38068
marklopervega <mark@vegafederal.com>
2026-07-02 10:39:23 -0400

    Lot's of little things, still fighting versioning, and a little folder structure changes.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-oxc'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: 'src',
   build: {
-    outDir: '../dist',
+    outDir: 'dist',
     emptyOutDir: true,
   },
 })
